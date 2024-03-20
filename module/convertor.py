2f3d961035111947f4a3d7577405960175486ae5
uthree <uthree271828@gmail.com>
2024-03-20 14:09:16 +0900

    fix

--- module/convertor.py
+++ module/convertor.py
@@ -58,16 +58,16 @@ class Convertor(nn.Module):
             # cross fade
             y_prev = torch.cat([output_buffer, torch.zeros(1, chunk_size, device=device)], dim=1)[:, chunk_size:]
             alpha = torch.cat([
-                torch.linspace(0.0, 1.0, buffer_size - chunk_size, device=device),
+                torch.zeros(buffer_size - chunk_size * 2, device=device),
+                torch.linspace(0.0, 1.0, chunk_size, device=device),
                 torch.ones(chunk_size, device=device)]).unsqueeze(0)
 
-            output = y_prev * (1 - alpha) + y * alpha
-            output_buffer = y
+            output_buffer = y_prev * (1 - alpha) + y * alpha
 
             # cut center
             left = buffer_size // 2 - chunk_size // 2
             right = buffer_size // 2 + chunk_size // 2
-            chunk = output[:, left:right]
+            chunk = output_buffer[:, left:right]
 
         buffer = (input_buffer, output_buffer, norm_buffer)
         return chunk, buffer
